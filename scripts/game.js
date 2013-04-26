1939eb7741307405b1bcbfe742c73345f49c7cb0
cfanguy <cfanguy@gmail.com>
2013-04-25 22:36:50 -0500

    Added Snake enemy to game and Bandit image to folder. Need to fix issues with Snake.

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,31 @@
 var gameComplete = false, gameOver = false;
 var rects = [], spikes = [];
 var diam = diamond(560, 360, 20, 20);
+var snake = snakeEnemy(420, 285, 30,15);
 var player;
 var level;
 var blockCount;
 
 // Record which key codes are currently pressed
 var keys = {};
-document.onkeydown = function (e) { keys[e.which] = true };
-document.onkeyup = function (e) { keys[e.which] = false };
+document.onkeydown = function (e) { keys[e.which] = true; };
+document.onkeyup = function (e) { keys[e.which] = false; };
 var direction = { right: false, left: false, up: false };
 
 // set initial player img
 var img = document.getElementById("player_r");
 
 function rect(x, y, w, h) {
-    return { x: x, y: y, w: w, h: h }
+    return { x: x, y: y, w: w, h: h };
 }
 function diamond(x, y, w, h) {
-    return { x: x, y: y, w: w, h: h }
+    return { x: x, y: y, w: w, h: h };
 }
 function spike(x, y, w, h) {
-    return { x: x, y: y, w: w, h: h }
+    return { x: x, y: y, w: w, h: h };
+}
+function snakeEnemy(x, y, w, h) {
+    return { x: x, y: y, w: w, h: h };
 }
 
 
@@ -59,6 +63,10 @@ function startGame() {
     player = rect(20, 20, 26, 34);
     player.velocity = { x: 0, y: 0 };
     player.onFloor = false;
+    
+    snake.velocity = { x: 0, y: 0 };
+    snake.velocity.x = -2;
+    snake.velocity.y = 1;
 
     // initial platform for player
     rects.push(rect(20, 100, 20, 20));
@@ -154,80 +162,124 @@ function setLevelBlocks() {
 }
 
 
-// Returns true if and only if a and b overlap
+// returns true if and only if a and b overlap
 function overlapTest(a, b) {
     return a.x < b.x + b.w && a.x + a.w > b.x &&
-         a.y < b.y + b.h && a.y + a.h > b.y
+         a.y < b.y + b.h && a.y + a.h > b.y;
 }
 
 
-// Move the player p along vx then along vy, but only move
+// move the player p along vx then along vy, but only move
 // as far as we can without colliding with a solid rectangle
-function move(p, vx, vy) {
-    // Move rectangle along x axis
-	for (var i = 0; i < rects.length; i++) {
-		var c = { x: p.x + vx, y: p.y, w: p.w, h: p.h }
-		if (overlapTest(c, rects[i])) {
-		    if (vx < 0)
-		        vx = rects[i].x + rects[i].w - p.x
-			else if (vx > 0)
-			    vx = rects[i].x - p.x - p.w
-		}
-		if (overlapTest(c, diam)) {
-		    gameComplete = true;
-		}
-	}
-	p.x += vx
+function movePlayer(p, vx, vy) {
+    // move player along x axis
+    for (var i = 0; i < rects.length; i++) {
+            var c = { x: p.x + vx, y: p.y, w: p.w, h: p.h };
+            if (overlapTest(c, rects[i])) {
+                if (vx < 0)
+                    vx = rects[i].x + rects[i].w - p.x;
+                    else if (vx > 0)
+                        vx = rects[i].x - p.x - p.w;
+            }
+            if (overlapTest(c, diam)) {
+                gameComplete = true;
+            }
+    }
+    p.x += vx;
 
-	// Move rectangle along y axis
-	for (var i = 0; i < rects.length; i++) {
-		var c = { x: p.x, y: p.y + vy, w: p.w, h: p.h }
-		if (overlapTest(c, rects[i])) {
-		    if (vy < 0)
-		        vy = rects[i].y + rects[i].h - p.y
-			else if (vy > 0)
-			    vy = rects[i].y - p.y - p.h
-		}
-		if (overlapTest(c, diam)) {
-		    gameComplete = true;
-		}
-	}
-	p.y += vy
+    // move player along y axis
+    for (var i = 0; i < rects.length; i++) {
+            var c = { x: p.x, y: p.y + vy, w: p.w, h: p.h };
+            if (overlapTest(c, rects[i])) {
+                if (vy < 0)
+                    vy = rects[i].y + rects[i].h - p.y;
+                    else if (vy > 0)
+                        vy = rects[i].y - p.y - p.h;
+            }
+            if (overlapTest(c, diam)) {
+                gameComplete = true;
+            }
+    }
+    p.y += vy;
 
     // spike collisions
-	for (var n = 0; n < spikes.length; n++) {
-	    var c = { x: p.x + vx, y: p.y, w: p.w, h: p.h }
-	    if (overlapTest(c, spikes[n])) {
-	        gameOver = true;
-	    }
-	}
+    for (var n = 0; n < spikes.length; n++) {
+        var c = { x: p.x + vx, y: p.y, w: p.w, h: p.h };
+        if (overlapTest(c, spikes[n])) {
+            gameOver = true;
+        }
+    }
+    
+    // snake collision
+    if (overlapTest(c, snake)) {
+        gameOver = true;
+    }
+}
+
+
+// move the enemy along vx then along vy, but only move
+// as far as we can without colliding with a solid rectangle
+function moveEnemy(p, vx, vy) {
+    // move enemy along x axis
+    for (var i = 0; i < rects.length; i++) {
+        var c = { x: p.x + vx, y: p.y, w: p.w, h: p.h };
+        if (overlapTest(c, rects[i])) {
+            if (vx < 0) {
+                vx = rects[i].x + rects[i].w - p.x;
+                snake.velocity.x *= -1;
+            }
+            else if (vx > 0) {
+                vx = rects[i].x - p.x - p.w;
+                snake.velocity.x *= -1;
+            }
+        }
+    }
+    p.x += vx;
+
+    // move enemy along y axis
+    for (var i = 0; i < rects.length; i++) {
+        var c = { x: p.x, y: p.y + vy, w: p.w, h: p.h };
+        if (overlapTest(c, rects[i])) {
+            if (vy < 0) {
+                vy = rects[i].y + rects[i].h - p.y;
+            }
+            else if (vy > 0) {
+                vy = rects[i].y - p.y - p.h;
+            }
+        }
+        else
+        {
+            snake.velocity.x *= -1;
+        }
+    }
+    //p.y += vy;
 }
 
 
 // set the left button move event
 function left(e,bool) {
-	if(e.target.localName != 'select'){
+    if(e.target.localName != 'select'){
         e.preventDefault(); 
     }
-	direction.left = bool;
+    direction.left = bool;
 }
 
 
 // set the right button move event
 function right(e,bool) {
-	if(e.target.localName != 'select'){
+    if(e.target.localName != 'select'){
         e.preventDefault(); 
     }
-	direction.right = bool;
+    direction.right = bool;
 }
 
 
 // set the jump button move event
 function jump(e,bool) {
-	if(e.target.localName != 'select'){
+    if(e.target.localName != 'select'){
         e.preventDefault(); 
     }
-	direction.up= bool;
+    direction.up= bool;
 }
 
 // Updates the state of the game for the next frame
@@ -240,76 +292,80 @@ function update() {
     }
     player.velocity.y += 1; // Acceleration due to gravity
 
-	// Move the player and detect collisions
-	var expectedY = player.y + player.velocity.y
-	move(player, player.velocity.x, player.velocity.y);
-	player.onFloor = (expectedY > player.y);
-	if (expectedY != player.y) {
-	    player.velocity.y = 0;
-	}
+    // Move the player and detect collisions
+    var expectedY = player.y + player.velocity.y;
+    movePlayer(player, player.velocity.x, player.velocity.y);
+    moveEnemy(snake, snake.velocity.x, snake.velocity.y);
+    player.onFloor = (expectedY > player.y);
+    if (expectedY != player.y) {
+        player.velocity.y = 0;
+    }
 
-	// Only jump when we're on the floor
-	if (player.onFloor && (keys[87] || direction.up)) {
-	    player.velocity.y = -13;
-	}
+    // Only jump when we're on the floor
+    if (player.onFloor && (keys[87] || direction.up)) {
+        player.velocity.y = -13;
+    }
 }
 
 
 // Renders a frame
 function draw() {
-	var c = document.getElementById('screen').getContext('2d');
+    var c = document.getElementById('screen').getContext('2d');
 
-	// Draw background
-	c.fillStyle = '#000';
-	c.fillRect(0, 0, c.canvas.width, c.canvas.height);
-	
-	// Draw player
-	if (player.velocity.x > 0)
-		img = document.getElementById("player_r");
-	else
-	    if (player.velocity.x < 0)
-			img = document.getElementById("player_l");
-	c.drawImage(img, player.x - 6, player.y - 5);
+    // Draw background
+    c.fillStyle = '#000';
+    c.fillRect(0, 0, c.canvas.width, c.canvas.height);
 
-	// Draw levels
-	var blImg = document.getElementById("block");
-	for (var i = 0; i < rects.length; i++) {
-		var r = rects[i];
-		c.drawImage(blImg, r.x, r.y);
-	}
+    // Draw player
+    if (player.velocity.x > 0)
+            img = document.getElementById("player_r");
+    else
+        if (player.velocity.x < 0)
+            img = document.getElementById("player_l");
+    c.drawImage(img, player.x - 6, player.y - 5);
+
+    // Draw levels
+    var blImg = document.getElementById("block");
+    for (var i = 0; i < rects.length; i++) {
+        var r = rects[i];
+        c.drawImage(blImg, r.x, r.y);
+    }
 
     // draw diamond
-	var dImg = document.getElementById('diamond');
-	c.drawImage(dImg, diam.x, diam.y);
+    var dImg = document.getElementById('diamond');
+    c.drawImage(dImg, diam.x, diam.y);
 
     // draw spikes
-	var sImg = document.getElementById('spike');
-	for (var n = 0; n < spikes.length; n++) {
-	    var s = spikes[n];
-	    c.drawImage(sImg, s.x, s.y);
-	}
+    var sImg = document.getElementById('spike');
+    for (var n = 0; n < spikes.length; n++) {
+        var s = spikes[n];
+        c.drawImage(sImg, s.x, s.y);
+    }
+    // draw snake
+    var snImg = document.getElementById('snake');
+    c.drawImage(snImg, snake.x, snake.y);
 
-	if (gameComplete) {
-	    if (level == 3) {
-	        var cImg = document.getElementById('complete');
-	        c.drawImage(cImg, 100, 60);
-	    }
-	    else {
-	        var cImg = document.getElementById('next_level');
-	        c.drawImage(cImg, 100, 60);
-	        level++;
+    if (gameComplete) {
+        if (level == 3) {
+            var cImg = document.getElementById('complete');
+            c.drawImage(cImg, 100, 60);
+        }
+        else {
+            var cImg = document.getElementById('next_level');
+            c.drawImage(cImg, 100, 60);
+            level++;
 
-	        setTimeout(reset, 1500);
-	    }
-	};
+            setTimeout(reset, 1500);
+        }
+    };
 
-	if (gameOver) {
-	    var gImg = document.getElementById('gameover');
-	    c.drawImage(gImg, 100, 60);
+    if (gameOver) {
+        var gImg = document.getElementById('gameover');
+        c.drawImage(gImg, 100, 60);
 
-	    var dImg = document.getElementById('player_dead');
-	    c.drawImage(dImg, player.x - 6, player.y - 5);
-	};
+        var dImg = document.getElementById('player_dead');
+        c.drawImage(dImg, player.x - 6, player.y - 5);
+    };
 }
 
 
@@ -334,4 +390,4 @@ window.onload = function () {
 
     setClickEvent();
     startGame();
-}
+};
